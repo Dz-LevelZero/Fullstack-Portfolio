084f60ee7297db670ed265f03dd7d73bbe6c02cc
Samy <dev.exelight@gmail.com>
2022-02-21 15:45:05 +0100

    Header Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,18 +23,18 @@ const Navbar = () => {
       </ul>
 
       <div className="app__navbar-menu">
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+          <HiMenuAlt4 onClick={() => setToggle((true))}/>
 
           {toggle && (
             <motion.div
               whileInView={{ x: [300, 0] }}
               transition={{ duration: 0.85, ease: "easeOut" }}
             >
-              <HiX onClick={() => setToggle(false)}/>
+              <HiX onClick={() => setToggle((prev) => !prev)}/>
               <ul>
                 {["home", "about", "work", "skills", "contact"].map((item) => (
                   <li key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                    <a href={`#${item}`} onClick={() => setToggle((prev) => !prev)}>{item}</a>
                   </li>
                 ))}
               </ul>
